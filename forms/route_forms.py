076b91a28626c6cb59d427fc139836c43843bd0d
Алексеев Пётр <qqwettykovich@gmail.com>
2021-05-28 19:07:55 +0300

    hotfix 1.2

--- forms/route_forms.py
+++ forms/route_forms.py
@@ -3,6 +3,16 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class AddNewRouteForm(FlaskForm):
+    """
+    Форма для добавления маршрута
+
+    :param name: название маршрута
+    :param time_hours: время маршрута
+    :param budget: бюджет маршрута
+    :param remarks: заметки для маршрута
+    :param coordinates: координаты точек маршрута
+    :param submit: сохранить форму(маршрут)
+    """
     name = StringField(validators=[DataRequired()])
     time_hours = StringField()
     budget = StringField()
