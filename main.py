727302326f430ed731db971d262d268f66818f5a
Алексеев Пётр <qqwettykovich@gmail.com>
2021-05-28 18:56:24 +0300

    Hotfix

--- main.py
+++ main.py
@@ -17,11 +17,22 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    :param user_id: id пользователя
+    :return: db_sess.query(User).get(user_id)
+    """
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
 
 def main():
+    """
+    Рендер сайта
+
+    :param db_session: база данных
+    :param port: порт
+    :return: app.run()
+    """
     db_session.global_init("db/user_data.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
@@ -29,11 +40,23 @@ def main():
 
 @app.route("/")
 def index():
+    """
+    Начальная страница
+
+    :return: rendering index page
+    """
     return render_template("index.html", title="ShareMyTrip")
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    """
+    Страница логина
+
+    :param form: неодходимая форма
+    :param db_sess: db_sess = db_session.create_session()
+    :return: rendering autorisation page
+    """
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -49,6 +72,13 @@ def login():
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
+    """
+    Страница регистрации
+
+    :param form: неодходимая форма
+    :param db_sess: db_sess = db_session.create_session()
+    :return: rendering registration page
+    """
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.repeat.data:
@@ -70,6 +100,15 @@ def signup():
 @login_required
 @app.route("/profile", methods=["POST", "GET"])
 def profile():
+    """
+    Страница профиля
+
+    :param form: неодходимая форма
+    :param db_sess: db_sess = db_session.create_session()
+    :param user: db_sess.query(User).get(current_user.id)
+    :return: rendering profile page
+
+    """
     form = ProfileButtons()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -96,12 +135,25 @@ def profile():
 @login_required
 @app.route("/show_main")
 def show_main():
+    """
+    Отображние карты
+
+    :return: rendering map page
+    """
     return render_template("show_map.html", title="Карта")
 
 
 @login_required
 @app.route("/all_routes")
 def all_routes():
+    """
+    Страница со всеми маршрутами
+
+    :param routes: маршруты
+    :param users: пользователи
+    :param db_sess: = db_session.create_session()
+    :return: rendering all routes page
+    """
     db_sess = db_session.create_session()
     routes = db_sess.query(Route).all()
     users = db_sess.query(User).all()
@@ -112,6 +164,14 @@ def all_routes():
 @login_required
 @app.route("/my_routes/<int:id_author>")
 def my_routes(id_author):
+    """
+    Мои маршруты
+
+    :param id_author: автор
+    :param db_sess: = db_session.create_session()
+    :param routes: маршруты
+    :return: rendering my routes page
+    """
     db_sess = db_session.create_session()
     routes = db_sess.query(Route).filter(Route.author == id_author).all()
     return render_template("my_routes.html", title="Мои маршруты",
@@ -121,6 +181,14 @@ def my_routes(id_author):
 @login_required
 @app.route("/route/<int:id>")
 def route(id):
+    """
+    Просмотр маршрута
+
+    :param id: id маршрута
+    :param route: маршрут
+    :param db_sess: = db_session.create_session()
+    :return: rendering route view page
+    """
     db_sess = db_session.create_session()
     route = db_sess.query(Route).filter(Route.id == id).first()
     return render_template("route.html", title='Просмотр маршрута', r=route)
@@ -129,6 +197,13 @@ def route(id):
 @login_required
 @app.route("/add_new_route", methods=["POST", "GET"])
 def add_route():
+    """
+    Добавление маршрута
+
+    :param form: неодходимая форма и все параметры
+    :param db_sess: = db_session.create_session()
+    :return: rendering add route page
+    """
     form = AddNewRouteForm()
     if form.validate_on_submit():
         coords = form.coordinates.data.split("\r\n")
@@ -160,6 +235,14 @@ def add_route():
 @login_required
 @app.route("/route/<int:id>/map")
 def route_map(id):
+    """
+    Маршрут на карте
+
+    :param id: id маршрута
+    :param db_sess: = db_session.create_session()
+    :param route: маршрут
+    :return: rendering route map page
+    """
     db_sess = db_session.create_session()
     route = db_sess.query(Route).filter(Route.id == id).first()
     return render_template("route_map.html", title="Карта", r=route)
@@ -168,12 +251,22 @@ def route_map(id):
 @login_required
 @app.route("/confirm_logout")
 def confirm_logout():
+    """
+    Подтверждение выхода из аккаунта
+
+    :return: rendering logout page
+    """
     return render_template("confirm_logout.html", title="Выход")
 
 
 @login_required
 @app.route("/logout")
 def logout():
+    """
+    Выход из профиля
+
+    :return: redirect("/")
+    """
     logout_user()
     return redirect("/")
 
